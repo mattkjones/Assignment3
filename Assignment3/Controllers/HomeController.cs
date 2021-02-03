12c882054d42a2842b8b196756c891a25f6ec776
mattkjones <mkj8@comcast.net>
2021-02-03 14:54:27 -0700

    Add project files.

---
+++ Assignment3/Controllers/HomeController.cs
@@ -0,0 +1,68 @@
+﻿using Assignment3.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment3.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Podcast()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult MovieForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult MovieForm(Films addMovie)
+        {
+            if (ModelState.IsValid)
+            {
+                TempStorage.AddMovie(addMovie);
+                return View("Confirmation", addMovie);
+            }
+            else
+            {
+                return View();
+            }
+            
+        }
+
+        public IActionResult MovieCollection()
+        {
+            return View(TempStorage.MovieList);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
